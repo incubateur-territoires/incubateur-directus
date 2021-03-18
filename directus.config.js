5142e9b9be960446f70837dcc7c6c0d2a0b645ed
Jonathan Pichot <jonathan@pichot.us>
2021-03-18 14:17:45 +0100

    set port

--- directus.config.js
+++ directus.config.js
@@ -1,7 +1,8 @@
 // Heroku to Directus mappings
 module.exports = {
-  DB_CONNECTION_STRING:
-    process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL,
+  PORT: 80,
+  
+  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || process.env.DATABASE_URL,
   RATE_LIMITER_REDIS: process.env.RATE_LIMITER_REDIS || process.env.REDIS_URL,
   CACHE_REDIS: process.env.CACHE_REDIS || process.env.REDIS_URL,
   EMAIL_FROM: process.env.EMAIL_FROM || process.env.MAILGUN_SMTP_LOGIN,
